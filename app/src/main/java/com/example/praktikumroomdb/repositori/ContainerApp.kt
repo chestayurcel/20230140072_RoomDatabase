e517dca89573cebf8be05fd94891a01e283307e9
Yurcell <chestayurcel@gmail.com>
2025-11-27 13:20:37 +0700

    Menambahkan interface ContainerApp, kelas implementasi ContainerDataApp, dan kelas AplikasiSiswa untuk manajemen dependensi aplikasi

--- app/src/main/java/com/example/praktikumroomdb/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/praktikumroomdb/repositori/ContainerApp.kt
@@ -1,2 +1,21 @@
 package com.example.praktikumroomdb.repositori
 
+import android.app.Application
+import android.content.Context
+import com.example.praktikumroomdb.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
+class AplikasiSiswa : Application() {
+
+}
