f5eaaa571e96cd7eb387877169814ce95e3e5647
Yurcell <chestayurcel@gmail.com>
2025-11-27 12:53:23 +0700

    memindahkan dependensi testing dan debug ke akhir blok dependencies dan hapus versi Kotlin yang tidak digunakan dari libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,13 +50,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
     //icons
     implementation(libs.compose.icons)
     //Room
@@ -67,4 +60,11 @@ dependencies {
     implementation(libs.lifecycle.runtime.compose)
     // Navigasi
     implementation(libs.navigation.compose)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
