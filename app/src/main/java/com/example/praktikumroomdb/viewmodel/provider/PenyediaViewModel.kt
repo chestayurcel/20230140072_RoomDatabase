cadd8dc9b268eb5a762318a20dfcc5f510430cda
Yurcell <chestayurcel@gmail.com>
2025-11-27 14:02:24 +0700

    Menambahkan object PenyediaViewModel untuk menyediakan factory HomeViewModel dan EntryViewModel

--- app/src/main/java/com/example/praktikumroomdb/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikumroomdb/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,17 @@
 package com.example.praktikumroomdb.viewmodel.provider
 
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.praktikum9.viewmodel.EntryViewModel
+import com.example.praktikumroomdb.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
