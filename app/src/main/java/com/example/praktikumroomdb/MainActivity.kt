77ee786985caaa9fdc45ea2223202be63c2b15fe
Yurcell <chestayurcel@gmail.com>
2025-11-27 14:20:13 +0700

    Menambahkan file PetaNavigasi untuk mengatur navigasi aplikasi dan mengimplementasikan SiswaTopAppBar serta SiswaApp di MainActivity

--- app/src/main/java/com/example/praktikumroomdb/MainActivity.kt
+++ app/src/main/java/com/example/praktikumroomdb/MainActivity.kt
@@ -20,28 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             PraktikumRoomDBTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PraktikumRoomDBTheme {
-        Greeting("Android")
     }
 }
